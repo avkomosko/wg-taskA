64ea12e8acec2a8d22243a270a029a10623c8721
Aliaksei <avkomosko@gmail.com>
2021-09-07 23:13:29 +0300

    feat: implement function and finish task

--- A.js
+++ A.js
@@ -1,33 +1,51 @@
-let input = [1,3,1,4,5,8,9,10,12];
+let input = [1,3,1,4,5,8,9,10,11,12,15];
 
 function solve(input) {
   let set = new Set(input);
   if (set.has(',')) {
     set.delete(',');
   }
+
   let array = Array.from(set);
   array.sort((a,b)=> a - b);
-  console.log(array);
   
   function findSequence(arr) {
-    let stack = [];
-    for (let i = 1; i < arr.length; i++) {
-      stack.push(arr[0]);
-      if ((arr[i] !== stack[stack.length-1]+1 && arr[i]+1 === arr[i+1]) || (i === 0 && arr[i]+1 === arr[i+1])) {
-        sequence.push('start');
+    let sequence =[];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== arr[i-1]+1 && arr[i]+1 === arr[i+1] && i!== 0) {
+        sequence.push(0);
       } else if (arr[i] === arr[i-1]+1 && arr[i]+1 !== arr[i+1]) {
-        sequence.push('end');
+        sequence.push(2);
       } else if (arr[i] === arr[i-1]+1 && arr[i]+1 === arr[i+1]){
-        sequence.push('sequence');
+        sequence.push(1);
       } else {
-        return sequence.push(0);
+        sequence.push(-1);
       }
     }
     return sequence;
   }
- 
-  console.log(findSequence(array));
-  return array;
+  
+  function makeSequence(arr) {
+    let arr2 = findSequence(arr);
+    do {
+      let start = 0;
+      let end = 0;
+      for (let i = 0; i < arr.length; i++) {
+        if (arr2[i] === 0) {
+          start = i;
+        } else if (arr2[i] === 2) {
+          end = i;
+        }
+      }
+      if (end !== 0) {
+        arr.splice(start, end-start+1, `${arr[start]}-${arr[end]}`);
+        arr2.splice(start, end-start+1, -1);
+      } 
+    } while (arr2.includes(0));
+  }
+
+  makeSequence(array);
+  return array.join(',');
 }
 solve(input);
 
